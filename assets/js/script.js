16d60b4d4a411e1b9c6d367c850ee640d8c22c82
MartsyniukOlena <>
2023-10-24 12:09:43 +0000

    Add variables and function to generate computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,20 @@ let startButton = document.getElementById("start-button");
 startButton.addEventListener("click", function () {
     hideWelcomeMessage();
 });
+
+// Possible choices
+let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+let computerChoice = "";
+let userChoice = "";
+let userScore = 0;
+let computerScore = 0;
+let tries = 5;
+
+// Function to generate computer's choice
+
+function generateComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 5);
+    return choices[randomChoice];
+    let computerChoice = document.getElementById("computer-choice");
+}
